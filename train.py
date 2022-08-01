38adccf690f62509922b07eb1876dd63b18bb0a7
enwaonumah <enwaonumah@path-robotics.com>
2022-08-01 09:19:49 -0400

    commit-v4

--- train.py
+++ train.py
@@ -1,9 +1,28 @@
 import numpy as np
 import tensorflow as tf
 import valohai
- 
-input_path = valohai.inputs('datasets').path()
-#'valohai-test/mnist.npz'
+
+input_path = {
+    valohai.inputs('datasets').path() #'valohai-test/mnist.npz'
+}
+
+valohai.prepare(
+    step="train-model",
+    image="tensorflow/tensorflow:2.6.0",
+    default_inputs= input_path,
+    default_parameters={
+        'learning_rate': 0.001,
+        'epoch': 10,
+    },
+)
+
+def log_metadata(epoch, logs):
+    with valohai.logger() as logger:
+        logger.log('epoch', epoch)
+        logger.log('accuracy', logs['accuracy'])
+        logger.log('loss', logs['loss'])
+
+
 with np.load(input_path, allow_pickle=True) as f:
     x_train, y_train = f['x_train'], f['y_train']
     x_test, y_test = f['x_test'], f['y_test']
@@ -24,7 +43,16 @@ model.compile(optimizer=optimizer,
               metrics=['accuracy'])
  
 model.fit(x_train, y_train, epochs= valohai.parameters('epoch').value)
+
+callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=log_metadata)
+model.fit(x_train, y_train, epochs=valohai.parameters('epoch').value, callbacks=[callback])
+
+test_loss, test_accuracy = model.evaluate(x_test,  y_test, verbose=2)
  
+with valohai.logger() as logger:
+    logger.log('test_accuracy', test_accuracy)
+    logger.log('test_loss', test_loss)
+
 model.evaluate(x_test,  y_test, verbose=2)
  
 output_path = valohai.outputs().path('model.h5')
